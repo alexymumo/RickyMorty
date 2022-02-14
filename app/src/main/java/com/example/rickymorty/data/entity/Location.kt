a3a403a69a0a9e6b41708569872836a4d14b3079
Alex-mumo <alexymumo2000@gmail.com>
2022-02-15 00:26:17 +0300

    initial commit

---
+++ app/src/main/java/com/example/rickymorty/data/entity/Location.kt
@@ -0,0 +1,6 @@
+package com.example.rickymorty.data.entity
+
+data class Location(
+    val name: String,
+    val url: String
+)
---
+++ app/src/main/java/com/example/rickymorty/data/entity/Origin.kt
@@ -0,0 +1,6 @@
+package com.example.rickymorty.data.entity
+
+data class Origin(
+    val name: String,
+    val url: String
+)
