febb4b44a32ee3a78b1581ccef2f65d37789e85b
Alex-mumo <alexymumo2000@gmail.com>
2022-02-16 14:00:11 +0300

    update

--- app/src/main/java/com/example/rickymorty/di/CharacterModule.kt
+++ app/src/main/java/com/example/rickymorty/di/CharacterModule.kt
@@ -7,7 +7,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -27,6 +27,6 @@ object CharacterModule {
     fun provideRetrofit(): Retrofit.Builder{
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
     }
 }
