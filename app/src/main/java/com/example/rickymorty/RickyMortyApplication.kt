43eae54c09b386272f8bd84f53316b931540c37e
Alex-mumo <alexymumo2000@gmail.com>
2022-02-17 23:44:47 +0300

    Added README.md

--- app/src/main/java/com/example/rickymorty/RickyMortyApplication.kt
+++ app/src/main/java/com/example/rickymorty/RickyMortyApplication.kt
@@ -4,6 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RickyMortyApplication: Application() {
-
-}
+class RickyMortyApplication: Application()
