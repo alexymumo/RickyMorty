f6d8d7b7bb9053fcc8434ad24d373634a10ddc71
Alex-mumo <alexymumo2000@gmail.com>
2022-02-18 16:54:04 +0300

    working with composables

---
+++ README.md
@@ -0,0 +1,31 @@
+## RickyMorty App
+-Is an android application that consumes RickyMorty Api to fetch list of movie characters. It is built using MVVM Architecture, Kotlin programming language
+ and Jetpack Compose.
+-Minimum API Level: 21 [Supports Over 84% Devices](https://developer.android.com)
+-Build System: [Gradle](https://gradle.org/)
+
+## Table of Contents
+-[Prerequisite](#prerequisite)
+-[Libraries](#libraries)
+-[Screeenshots](#screenshots)
+
+## Prerequisite
+-Inorder to run the application you need to [clone].
+-Install Android studio or Intellij Idea
+
+## Libraries
+- [Jetpack Compose](https://developer.android.com/jetpack) - Declarative way of building user interfaces(UI)
+- [Jetpack components](https://developer.android.com/jetpack)
+- [Viewmodel](https://developer.android.com/topic/libraries/architecture/viewmodel) - Manage UI related data in  a lifecycle conscious way .
+- [Data Binding](https://developer.android.com/topic/libraries/data-binding) - Support library that allows binding of UI components in  layouts to data sources.
+- [Coil](https://coil) - Image Loading Library.
+- [Gson]() -
+- [Retrofit]() - 
+- []
+- [kotlinx.coroutines](https://github.com/Kotlin/kotlinx.coroutines) - Library Support for coroutines.
+- [Hilt](- [Jetpack](https://zaidzakir.medium.com/a-simple-android-app-using-mvvm-dagger-hilt-e9f45381f1bc) - Dependency Injection Library
+
+## Screenshots
+<img src="/resources/architecture.png" width="300%">
+
+ 
--- app/src/main/java/com/example/rickymorty/MainActivity.kt
+++ app/src/main/java/com/example/rickymorty/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.rickymorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rickymorty.ui.composables.MainScreen
 import com.example.rickymorty.ui.theme.RickyMortyTheme
@@ -15,11 +22,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RickyMortyTheme {
-                MainScreen()
-                //Surface(color = MaterialTheme.colors.surface) {
-                //}
+                Surface(color = MaterialTheme.colors.surface) {
+                    Scaffold(content = { ShowMainScreen() })
+                }
             }
         }
+    }
+}
+@Composable
+fun ShowMainScreen(){
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        MainScreen()
     }
 }
 
@@ -27,6 +40,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     RickyMortyTheme {
-        MainScreen()
+        ShowMainScreen()
     }
 }
