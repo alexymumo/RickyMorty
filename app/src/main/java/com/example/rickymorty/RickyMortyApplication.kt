d6702edef7475d897768d76a558c123c87149dd8
Alex-mumo <alexymumo2000@gmail.com>
2022-02-18 17:33:19 +0300

    Added Timber

--- app/src/main/java/com/example/rickymorty/RickyMortyApplication.kt
+++ app/src/main/java/com/example/rickymorty/RickyMortyApplication.kt
@@ -2,6 +2,12 @@ package com.example.rickymorty
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class RickyMortyApplication: Application()
+class RickyMortyApplication: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
