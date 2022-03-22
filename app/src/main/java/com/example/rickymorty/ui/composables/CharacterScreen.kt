3aba8d9f1902f43b2f9ea741d2d58aa24811278d
Alex-mumo <alexymumo2000@gmail.com>
2022-03-22 12:28:35 +0300

    added composables

--- app/src/main/java/com/example/rickymorty/ui/composables/CharacterScreen.kt
+++ app/src/main/java/com/example/rickymorty/ui/composables/CharacterScreen.kt
@@ -1,6 +1,9 @@
 package com.example.rickymorty.ui.composables
 
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,6 +14,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
@@ -24,7 +28,104 @@ import com.example.rickymorty.viewmodel.CharacterViewModel
 
 @Composable
 fun CharacterInfo(){
-    val characterViewModel = viewModel(modelClass = CharacterViewModel::class.java)
+   
+   
+}
+
+@Composable
+fun CharacterCard(character: Character, onClick:() -> Unit) {
+    Card(modifier = Modifier
+        .padding(top = 5.dp, bottom = 5.dp, start = 5.dp, end = 5.dp)
+        .fillMaxWidth()
+        .clickable(onClick = onClick),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 12.dp
+    ) {
+        Row(modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+        ) {
+            Surface(
+                modifier = Modifier.size(130.dp),
+                shape = RoundedCornerShape(12.dp),
+                color = MaterialTheme.colors.surface.copy(alpha = 0.2f)) {
+
+                val image = rememberImagePainter(data = character.image)
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier.height(100.dp)
+                        .clip(RoundedCornerShape(12.dp)),
+                    contentScale = ContentScale.Crop)
+            }
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RickyMortyTheme {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*val image = rememberImagePainter(data = character.image)
+Column {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 16.dp) {
+        Column {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier.height(200.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Name: ${character.name}", textAlign = TextAlign.Left, color = Color.Cyan)
+                Spacer(modifier = Modifier.padding(top = 5.dp))
+                Text(text = "Origin: ${character.species}", textAlign = TextAlign.Left, color = Color.Cyan)
+                Spacer(modifier = Modifier.padding(top = 5.dp))
+                Text(text = "Gender: ${character.gender}", textAlign = TextAlign.Left, color = Color.Cyan)
+            }
+        }
+    }
+}*/
+
+/* val characterViewModel = viewModel(modelClass = CharacterViewModel::class.java)
     val state by characterViewModel.state.collectAsState()
     LazyColumn {
         if (state.isEmpty()) {
@@ -43,42 +144,7 @@ fun CharacterInfo(){
             }
         }
     }
-}
-
-@Composable
-fun CharacterCard(character: Character) {
-    val image = rememberImagePainter(data = character.imageUrl)
-    Column {
-        Card(
-            modifier = Modifier.padding(8.dp),
-            shape = RoundedCornerShape(8.dp),
-            elevation = 16.dp) {
-            Column {
-                Image(
-                    painter = image,
-                    contentDescription = null,
-                    modifier = Modifier.height(200.dp),
-                    contentScale = ContentScale.Crop
-                )
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Name: ${character.name}", textAlign = TextAlign.Left, color = Color.Cyan)
-                    Spacer(modifier = Modifier.padding(top = 5.dp))
-                    Text(text = "Origin: ${character.species}", textAlign = TextAlign.Left, color = Color.Cyan)
-                    Spacer(modifier = Modifier.padding(top = 5.dp))
-                    Text(text = "Gender: ${character.gender}", textAlign = TextAlign.Left, color = Color.Cyan)
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RickyMortyTheme {
-        CharacterInfo()
-    }
-}
+    */
 
 
 
--- app/src/main/java/com/example/rickymorty/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/com/example/rickymorty/viewmodel/CharacterViewModel.kt
@@ -2,24 +2,22 @@ package com.example.rickymorty.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.rickymorty.data.local.datasources.CharacterPagingSource
 import com.example.rickymorty.data.local.entities.Character
-import com.example.rickymorty.data.local.repository.CharacterRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class CharacterViewModel @Inject constructor(private val characterRepository: CharacterRepository): ViewModel() {
-    private val _state = MutableStateFlow(emptyList<Character>())
-    val state:StateFlow<List<Character>>
-    get() = _state
-
-    init {
-        viewModelScope.launch {
-            val characters = characterRepository.getCharacters()
-            _state.value = characters
-        }
-    }
+class CharacterViewModel @Inject constructor(
+    private val characterPagingSource: CharacterPagingSource
+    ): ViewModel() {
+        val character: Flow<PagingData<Character>> = Pager(PagingConfig(pageSize = 20)) {
+            characterPagingSource
+        }.flow.cachedIn(viewModelScope)
+    
 }
