2339261dc9ec9bfe5d33152917e89358b4af35ea
alexymumo <alexymumo2000@gmail.com>
2022-03-30 12:51:05 +0300

    added api call

---
+++ data/src/main/java/com/example/data/network/utils/SafeApiCall.kt
@@ -0,0 +1,19 @@
+package com.example.data.network.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+open class SafeApiCall {
+
+    suspend fun <T> safeApiCall(
+        apiCall: suspend () -> T
+    ): Result<T> {
+        return withContext(Dispatchers.IO) {
+            try {
+                Result.success(apiCall.invoke())
+            } catch (throwable: Throwable) {
+                Result.failure(throwable)
+            }
+        }
+    }
+}
