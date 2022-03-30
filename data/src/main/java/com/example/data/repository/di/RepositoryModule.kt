099852905e9f1fa50b811d867441e371aabd2893
alexymumo <alexymumo2000@gmail.com>
2022-03-30 12:17:08 +0300

    added respository module

--- data/src/main/java/com/example/data/repository/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/repository/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.example.data.repository.di
 
+import com.example.data.repository.datasource.CharacterRepositoryImpl
+import com.example.domain.repositories.CharacterRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-
+    single<CharacterRepository> { CharacterRepositoryImpl(get()) }
 }
