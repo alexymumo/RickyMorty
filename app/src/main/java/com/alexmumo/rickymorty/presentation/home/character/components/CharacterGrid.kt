1a1f10747c65d4de6f5469b11dc51f5b63b722ff
alexymumo <alexymumo2000@gmail.com>
2022-03-31 10:34:27 +0300

    update

--- app/src/main/java/com/alexmumo/rickymorty/presentation/home/character/components/CharacterGrid.kt
+++ app/src/main/java/com/alexmumo/rickymorty/presentation/home/character/components/CharacterGrid.kt
@@ -1,13 +1,17 @@
 package com.alexmumo.rickymorty.presentation.home.character.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
 import com.alexmumo.rickymorty.domain.models.Character
 
 @Composable
@@ -21,6 +25,18 @@ fun GridList(character: Character, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@ExperimentalFoundationApi
 @Composable
-fun CharacterGrid() {
+fun CharacterGrid(
+    items: LazyPagingItems<Character>,
+    listState: LazyGridState = rememberLazyGridState()
+) {
+    LazyVerticalGrid(cells = GridCells.Fixed(2), state = listState) {
+        items(items.itemCount) { index ->
+            items[index]?.let {
+                GridList(character = it, modifier = Modifier.animateContentSize())
+            }
+        }
+    }
 }
