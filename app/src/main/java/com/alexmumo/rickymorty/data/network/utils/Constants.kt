a8722c0bf061d6959d89d3f32e6039d68c2892b2
alexymumo <alexymumo2000@gmail.com>
2022-03-31 15:12:15 +0300

    update

--- app/src/main/java/com/alexmumo/rickymorty/data/network/utils/Constants.kt
+++ app/src/main/java/com/alexmumo/rickymorty/data/network/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.alexmumo.rickymorty.data.network.utils
 
 object Constants {
-    const val BASE_URL = "https://rickandmortyapi.com/api"
+    const val BASE_URL = "https://rickandmortyapi.com/api/"
 }
