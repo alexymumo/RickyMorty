e2e84185139046d48c5a4acb0cf93cbc5a8dfbb0
alexymumo <alexymumo2000@gmail.com>
2022-03-31 15:40:48 +0300

    fixing bugs

--- app/src/main/java/com/alexmumo/rickymorty/presentation/home/character/components/CharacterCard.kt
+++ app/src/main/java/com/alexmumo/rickymorty/presentation/home/character/components/CharacterCard.kt
@@ -2,17 +2,20 @@ package com.alexmumo.rickymorty.presentation.home.character.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import com.alexmumo.rickymorty.domain.models.Character
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun CharacterUI(character: Character, modifier: Modifier = Modifier) {
@@ -25,20 +28,50 @@ fun CharacterUI(character: Character, modifier: Modifier = Modifier) {
             elevation = 8.dp
         ) {
             Row {
-                CharacterCard(character = character)
-                CharacterInfo(character = character, modifier = Modifier.fillMaxSize().padding(4.dp))
+                CharacterCard(
+                    character = character,
+                    characterImage = character.imageUrl
+                )
+                CharacterInfo(
+                    character = character,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun CharacterCard(character: Character) {
-    val imagePainter = rememberImagePainter(data = character.imageUrl)
+fun CharacterCard(characterImage: String, character: Character) {
+    CoilImage(
+        imageRequest =
+        ImageRequest
+            .Builder(LocalContext.current)
+            .data(characterImage)
+            .crossfade(true)
+            .build(),
+        alignment = Alignment.Center,
+
+        loading = {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        },
+        failure = {
+            Text(text = "Failed to load image")
+        }
+    )
+    /*val imagePainter = rememberImagePainter(data = character.imageUrl)
     Image(
         painter = imagePainter,
         contentDescription = null
     )
+
+     */
 }
 
 @Composable
