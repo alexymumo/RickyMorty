a1c51464dc19970037f425378c5fa9ec2c4748fb
alexymumo <alexymumo2000@gmail.com>
2022-04-02 15:26:14 +0300

    update

---
+++ app/src/main/java/com/alexmumo/rickymorty/data/network/models/details/CharacterDetailDto.kt
@@ -0,0 +1,26 @@
+package com.alexmumo.rickymorty.data.network.models.details
+
+import com.google.gson.annotations.SerializedName
+
+data class CharacterDetailDto(
+    @SerializedName("created")
+    val created: String?,
+    @SerializedName("episode")
+    val episode: List<Any>?,
+    @SerializedName("gender")
+    val gender: String?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("image")
+    val image: String?,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("origin")
+    val species: String?,
+    @SerializedName("status")
+    val status: String?,
+    @SerializedName("type")
+    val type: String?,
+    @SerializedName("url")
+    val url: String?
+)
