544f6755ee436f738b503c2662e3431521e9b8fc
alexymumo <alexymumo2000@gmail.com>
2022-04-02 16:00:16 +0300

    updated repository module

--- app/src/main/java/com/alexmumo/rickymorty/data/repository/di/RepositoryModule.kt
+++ app/src/main/java/com/alexmumo/rickymorty/data/repository/di/RepositoryModule.kt
@@ -1,9 +1,12 @@
 package com.alexmumo.rickymorty.data.repository.di
 
+import com.alexmumo.rickymorty.data.repository.datasource.CharacteDetailRepositoryImpl
 import com.alexmumo.rickymorty.data.repository.datasource.CharacterRepositoryImpl
+import com.alexmumo.rickymorty.domain.repositories.CharacterDetailRepository
 import com.alexmumo.rickymorty.domain.repositories.CharacterRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<CharacterRepository> { CharacterRepositoryImpl(get()) }
+    single<CharacterDetailRepository> { CharacteDetailRepositoryImpl(get()) }
 }
