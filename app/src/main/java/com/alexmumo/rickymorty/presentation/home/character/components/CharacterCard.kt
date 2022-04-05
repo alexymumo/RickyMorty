ddaff564d30ed40a4b86efa2cabcfa117b2ded8d
alexymumo <alexymumo2000@gmail.com>
2022-04-05 11:01:02 +0300

    update

--- app/src/main/java/com/alexmumo/rickymorty/presentation/home/character/components/CharacterCard.kt
+++ app/src/main/java/com/alexmumo/rickymorty/presentation/home/character/components/CharacterCard.kt
@@ -4,16 +4,24 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.* // ktlint-disable no-wildcard-imports
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.request.ImageRequest
 import coil.transform.Transformation
 import com.alexmumo.rickymorty.domain.models.Character
@@ -78,4 +86,51 @@ fun CharacterCard(
             Text(text = "Failed to load image")
         }
     )
+}
+
+@Composable
+fun CharacterInfo(
+    character: Character,
+    modifier: Modifier = Modifier,
+    alignment: Alignment.Horizontal = Alignment.Start
+) {
+    val color = when (character.status) {
+        "Alive" -> Color.Green
+        "Dead" -> Color.Red
+        else -> Color.Yellow
+    }
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        horizontalAlignment = alignment
+    ) {
+        Text(
+            text = character.name,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.h6,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontStyle = FontStyle.Normal
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = character.gender,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.caption,
+            maxLines = 1
+        )
+        Text(text = "Status", fontSize = 13.sp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(10.dp)
+                    .background(color = color, shape = CircleShape)
+            )
+            Text(text = character.status + " - " + character.species)
+        }
+    }
 }
