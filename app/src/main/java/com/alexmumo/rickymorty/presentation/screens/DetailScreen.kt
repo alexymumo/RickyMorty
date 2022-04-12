81441f161fc5c2f24ae0ed9b875e2f306ef6d066
alexymumo <alexymumo2000@gmail.com>
2022-04-12 15:27:11 +0300

    update

--- app/src/main/java/com/alexmumo/rickymorty/presentation/screens/DetailScreen.kt
+++ app/src/main/java/com/alexmumo/rickymorty/presentation/screens/DetailScreen.kt
@@ -1,16 +1,77 @@
 package com.alexmumo.rickymorty.presentation.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.alexmumo.rickymorty.presentation.home.viewmodel.DetailsViewModel
+import org.koin.androidx.compose.getViewModel
 
 @Composable
-fun DetailScreen() {
-    Text(text = "Hello world")
-}
+fun DetailScreen(
+    navController: NavController,
+    detailsViewModel: DetailsViewModel = getViewModel(),
+    id: Int,
+) {
+    val lazyListState = rememberLazyListState()
+    val detailState = detailsViewModel.state.value
+    if (detailState.isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Card(shape = CircleShape) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(10.dp)
+                )
+            }
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        detailState.characterDetail?.let { character ->
 
-@Preview
-@Composable
-fun DetailScreenPreview() {
-    DetailScreen()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = character.name,
+                        style = MaterialTheme.typography.h4,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = character.species,
+                        style = MaterialTheme.typography.h4,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/alexmumo/rickymorty/presentation/screens/SearchScreen.kt
+++ app/src/main/java/com/alexmumo/rickymorty/presentation/screens/SearchScreen.kt
@@ -1,27 +1,10 @@
 package com.alexmumo.rickymorty.presentation.screens
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.* // ktlint-disable no-wildcard-imports
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun SearchScreen(navController: NavController) {
-    var text by remember { mutableStateOf("") }
-    OutlinedTextField(
-        value = text,
-        maxLines = 1,
-        textStyle = TextStyle(color = Color.Cyan, fontWeight = FontWeight.Bold),
-        onValueChange = { text = it },
-        label = { Text(text = "Search") },
-        modifier = Modifier.padding(20.dp)
-    )
+fun SearchScreen(
+    navController: NavController
+) {
 }
-
-
